0341b743123d1862ac5fca5d0d35ae1e9946b830
B4khytu1y <bakhytulynurzhan@gmail.com>
2024-08-27 20:47:46 +0500

    migration

---
+++ target/classes/db/migration/V1__Create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Account (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    accountNumber VARCHAR(10) NOT NULL,
+    clientId VARCHAR(10) NOT NULL,
+    balance DECIMAL(19, 2) NOT NULL
+);
+
+CREATE TABLE Transaction (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    accountId BIGINT NOT NULL,
+    amount DECIMAL(19, 2) NOT NULL,
+    transactionType VARCHAR(10) NOT NULL,
+    FOREIGN KEY (accountId) REFERENCES Account(id)
+);
